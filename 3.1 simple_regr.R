b3848da8958913df5fa5573341e23cfe6d55db49
Zelimkhan-Gudiev <gudievzk@gmail.com>
2022-09-05 19:23:20 +0300

    1

--- 3.1 simple_regr.R
+++ 3.1 simple_regr.R
@@ -3,6 +3,10 @@ rm()
 
 #### Packages and librarys. ____________________________________________________________________________________________________ ####
 
+library(ggplot2)
+library(psych)
+library(dplyr)
+library(psych)
 
 
 #### Data preprocessing _______________________________________________________________________________________________________ ####
@@ -20,7 +24,7 @@ setwd("")
 # В этом уроке мы научимся рассчитывать коэффициенты корреляции и познакомимся с простой линейной регрессией.
 # Ссылка на скрипт урока:
 # Корреляция https://stepic.org/media/attachments/lesson/11508/cortest.R
-# Регрессия ???https://stepic.org/media/attachments/lesson/11508/simple_regr.R
+# Регрессия https://stepic.org/media/attachments/lesson/11508/simple_regr.R
 
 
 df  <- mtcars
@@ -142,3 +146,158 @@ ggplot(mtcars, aes(mpg, disp))+
 ggplot(mtcars, aes(mpg, disp))+
          geom_point()+
          geom_smooth(aes(col = factor(am)))
+
+##### 3.2 ####
+x <- test_data <- read.csv("https://stepic.org/media/attachments/course/129/fill_na_test.csv")
+####  Step 5 of 15 ####
+# не правильная функция 
+fill_na <- function(x) {
+  fit <- lm(y ~ x_1 + x_2, x)
+  x$y_full <- predict(fit, subset(x, y != "NA"))
+  x$y_full <- ifelse(is.na(x$y), x$y_full, x$y)
+  return(x)
+}
+
+# не правильная функция
+fill_na <- function(x) {
+  fit <- lm(y ~ x_1 + x_2, x)
+    x$y_full <- ifelse(is.na(x$y), predict(fit, x), x$y)
+  return(x)
+}
+
+fill_na(x)
+
+####  Step 6 of 15 ####
+mtcars <- mtcars
+names(mtcars)
+which(names(mtcars) %in% с(mtcars$wt, mtcars$mpg, mtcars$disp, mtcars$drat, mtcars$hp)
+df <- mtcars[, c(mtcars$wt, mtcars$mpg, mtcars$disp, mtcars$drat, mtcars$hp)]
+df <- mtcars[, c(mtcars$wt, mtcars$mpg, mtcars$disp, mtcars$drat, mtcars$hp)]
+which(mtcars, с(mtcars$wt, mtcars$mpg, mtcars$disp, mtcars$drat, mtcars$hp)
+
+df <- select(mtcars, wt, mpg, disp, drat, hp)
+#1
+model <- lm(wt ~ wt + mpg + disp + drat + hp, df)
+summary(model)
+Multiple R-squared:  0.8584,	Adjusted R-squared:  0.8374
+
+#2
+model <- lm(wt ~ wt + mpg + disp + drat, df)
+summary(model)
+Multiple R-squared:  0.8407,	Adjusted R-squared:  0.8236    
+
+#3
+model <- lm(wt ~ wt + mpg + disp + hp, df)
+summary(model)
+Multiple R-squared:  0.858,	Adjusted R-squared:  0.8428 
+
+#4
+model <- lm(wt ~ wt + mpg + drat + hp, df)
+summary(model)
+Multiple R-squared:  0.7804,	Adjusted R-squared:  0.7568 
+
+#5
+model <- lm(wt ~ wt + disp + drat + hp, df)
+summary(model)
+Multiple R-squared:  0.8038,	Adjusted R-squared:  0.7828 
+
+#6
+model <- lm(wt ~ mpg + disp + drat + hp, df)
+summary(model)
+Multiple R-squared:  0.8584,	Adjusted R-squared:  0.8374
+
+
+###
+
+df1 <- mtcars[,c("wt", "mpg", "disp", "drat", "hp")]
+fit_full <- lm(wt ~ ., data = df1)
+optimal_fit <-  step(fit_full, direction = 'backward')
+opt_summary <- summary(optimal_fit)
+attr(as.formula(opt_summary), "term.labels")
+
+
+### 
+
+summary(lm(wt ~ mpg + disp + drat + hp, df))
+
+#Coefficients:
+Estimate Std. Error t value Pr(>|t|)    
+(Intercept)  4.364821   0.963395   4.531 0.000107 ***
+  mpg         -0.079981   0.024787  -3.227 0.003272 ** 
+  disp         0.005085   0.001318   3.857 0.000645 ***
+  drat        -0.054984   0.206363  -0.266 0.791922    
+hp          -0.003519   0.001914  -1.839 0.076967 . 
+
+summary(lm(wt ~ mpg + disp + hp, df)) #убрал drat как не значащую
+
+#Coefficients:
+Estimate Std. Error t value Pr(>|t|)    
+(Intercept)  4.200313   0.727164   5.776 3.35e-06 ***
+  mpg         -0.082135   0.023040  -3.565  0.00133 ** 
+  disp         0.005245   0.001155   4.543 9.67e-05 ***
+  hp          -0.003702   0.001757  -2.107  0.04416 *
+  
+  
+  
+### 
+# ссылка на коммент ниже, кот-й помог решить без перебора вариантов:
+# https://stepik.org/lesson/11509/step/6?discussion=82172&reply=82441&unit=2532
+# подробней:
+# 1) установить пакет leaps
+install.packages('leaps')
+library(leaps)
+b <- regsubsets(wt~., data = df, nbest = 4)
+# #(все комбинации для 4-х независимых переменных + Intercept)
+plot(b, scale = "adjr2")
+# 4) выбираем независимые переменные из верхнего (наибольшее adjr2) значения графика и подставляем 
+их в модель:
+model <- lm(wt~mpg+disp+hp,df)
+# Ну и можно заглянуть в конец урока - файла (я не догадался ( ), тогда через 
+fit_full  <-  lm(wt ~., df)  # регрессия по всем независимым переменным
+optimal_fit <-  step(fit_full, direction = 'backward')
+
+
+###
+
+> vars <- c("mpg", "disp", "drat", "hp",
+            "mpg + disp", "mpg + drat", "mpg + hp",
+            "disp + drat", "disp + hp", "drat+ hp",
+            "mpg + disp + drat", "mpg + drat + hp",
+            "mpg + disp + hp", "disp + drat + hp",
+            "mpg + disp + drat + hp")
+> adj.r.sqrt <- sapply(vars, function(i) summary(lm(paste0("wt ~ ", i), df))$adj.r.squared)
+> names(adj.r.sqrt)[which.max(adj.r.sqrt)]
+
+# Много тут писалось о том, что надо идти либо по stepwise forward, либо по stepwise backward подходам, 
+# но эти подходы являются "жадными" и потому не гарантируют оптимального решения. 
+# С текущим знанием языка R, а также с помощью интернетов пришёл к такому "перебору". 
+# Его, разумеется, можно и улучшить, но задачу свою выполнил.
+
+df <- mtcars[,c("wt", "mpg", "disp", "drat", "hp")]
+cols <- c("mpg", "disp", "drat", "hp")
+max_adj_r <- 0
+els <- ""
+
+for(n in 1:length(cols)){
+  for(xs in combn(c("mpg", "disp", "drat", "hp"), n, simplify = F)){
+    frml = paste(xs, collapse = ' + ')
+    lin_mod = lm(eval(paste0("wt ~ ", frml)), df)
+    sum = summary(lin_mod)
+    print(paste(frml, sum$adj.r.squared))
+    if(sum$adj.r.squared > max_adj_r){
+      max_adj_r <- sum$adj.r.squared
+      els <- frml
+    }
+  }
+}
+print("")
+print(paste("Max adj. R^2 is", max_adj_r, "for elements", els))
+
+
+
+
+###
+install.packages("olsrr")
+library(olsrr)
+m <- lm(wt ~ ., data = df)
+ols_step_both_p(m)
