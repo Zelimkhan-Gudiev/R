0607e91cb72038f5350c679ca116e6c4bf66dacb
Zelimkhan-Gudiev <gudievzk@gmail.com>
2022-09-16 18:50:34 +0300

    1

--- Basics of data analysis in R (part 2)/1.3 apply2.r
+++ Basics of data analysis in R (part 2)/1.3 apply2.r
@@ -1,13 +1,33 @@
-# step 2 apply(x, 1, mean, ...) 
+remove(list = ls())
+rm()
+
+#### Packages and librarys. ____________________________________________________________________________________________________ ####
+library(ggplot2)
+library(psych)
+library(dplyr)
+library(readxl)
+library(ROCR)
+
+#### Data preprocessing _______________________________________________________________________________________________________ ####
+
+getwd()
+setwd("C:/Users/GudievZK/Desktop/GitHub/DF/")
+setwd("/Users/zelimkhan/Desktop/Data/GitHub/DF/")#
+
+yt <- read.csv2("yt.csv")
+yt <- read_xlsx("plan.xlsx")
+data(diamonds)
+
+#### step 2 apply(x, 1, mean, ...) ####
 head(airquality)
 ?airquality
 
 apply(airquality, 2, mean, na.rm = T)
 
-colMeans()
-colSums()
-rowMeans()
-rowSums()
+colMeans(airquality, na.rm = T)
+colSums(airquality, na.rm = T)
+rowMeans(airquality, na.rm = T)
+rowSums(airquality, na.rm = T)
 
 # step 3 
 set.seed(42)
@@ -55,3 +75,17 @@ norm_test_p <- apply(iris[, 1:4], 2,
 str(aov_result)
 aov_result$Petal.Length
 norm_test$Petal.Width
+
+
+#### Step 5 of 8  ####
+# ƒавайте завершим и слегка модифицируем задачу из предыдущей лекции. Ќапишите функцию get_negative_values, 
+# котора€ получает на вход dataframe произвольного размера. ‘ункци€ должна дл€ каждой переменной в данных провер€ть, 
+# есть ли в ней отрицательные значени€. ≈сли в переменной отрицательных значений нет, то эта переменна€ 
+# нас не интересует, дл€ всех переменных, в которых есть отрицательные значени€ мы сохраним их в виде списка 
+# или матрицы, если число элементов будет одинаковым в каждой переменной (смотри пример работы функции).
+
+# ≈сли чувствуете, что задачка не даетс€, ничего страшного, тут вс€ сложность отобрать только нужные колонки!
+# ѕройдите следующий урок, возможно вооружившись знани€ми, как нам отбирать колонки данных по некому условию 
+# вы слегкостью решите эту задачу!
+
+
