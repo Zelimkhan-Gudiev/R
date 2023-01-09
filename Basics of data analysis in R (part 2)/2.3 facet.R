39cc0368ebd5d1b5452023b8e1999797ad9c9f65
Zelimkhan-Gudiev <gudievzk@gmail.com>
2023-01-09 09:37:53 +0300

    1

--- Basics of data analysis in R (part 2)/2.3 facet.R
+++ Basics of data analysis in R (part 2)/2.3 facet.R
@@ -1,3 +1,6 @@
+library(ggplot2)
+library(dplyr)
+
 ggplot(diamonds, aes(carat))+ 
   geom_density(alpha = .1) + 
   facet_grid(cut ~ color)
@@ -24,3 +27,36 @@ ggplot(diamonds, aes(carat, price))+
   geom_smooth() +   
   facet_grid(color ~ .)
   
+# Step 7 of 10
+mpg_facet <- ggplot(mtcars, aes(mpg)) +
+  geom_dotplot() +
+  facet_grid(am ~ vs)
+
+# Step 8 of 10
+sl_wrap <- ggplot(iris, aes(Sepal.Length)) + 
+  geom_density() +
+  facet_wrap(~ Species)
+
+
+# Step 9 of 10
+my_plot <- ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
+  geom_point() +
+  geom_smooth() +
+  facet_wrap(~ Species)
+
+# Step 10 of 10
+myMovieData <- read.table("C:/Users/GudievZK/Desktop/GitHub/DF/myMovieData.csv", header = T, sep = ",") %>% as_tibble()
+myMovieData[, c(3, 4)] <- lapply(myMovieData[, c(3, 4)], factor)
+myMovieData[, c(3, 4)] <- lapply(select(myMovieData, Year, Type), factor)
+glimpse(myMovieData)
+
+my_plot <- ggplot(myMovieData, aes(Type, Budget)) +
+  geom_boxplot() +
+  facet_grid(. ~ Year) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  
+
+
+
+
+
--- Other Scripts/2022.12.21_pressReliz.R
+++ Other Scripts/2022.12.21_pressReliz.R
@@ -1,28 +1,41 @@
 library(tidyverse)
 library(readxl)
 library(dplyr)
-library("writexl")
+library(writexl)
 library(tidyr)
 
-df9 <- read_excel("C:/Users/GudievZK/Downloads/2021.12.22 press.xlsx", sheet = "9month", )
-df11 <- read_excel("C:/Users/GudievZK/Downloads/2021.12.22 press.xlsx", sheet = "11month")
+df9 <- read_excel("C:/Users/GudievZK/Desktop/GitHub/DF/2022.12.22 press.xlsx", sheet = "9month")
+df11 <- read_excel("C:/Users/GudievZK/Desktop/GitHub/DF/2022.12.22 press.xlsx", sheet = "11month")
+df12 <- read_excel("C:/Users/GudievZK/Desktop/GitHub/DF/2022.12.22 press.xlsx", sheet = "12month")
 f_names <- c("nameTz", "low", "status")
+
 df9[f_names] <- lapply(df9[f_names], factor)
 df9$`dateKontract` <- as.Date(df9$`dateKontract`)
 str(df9)
+
 df11[f_names] <- lapply(df11[f_names], factor)
 df11$`dateKontract` <- as.Date(df11$`dateKontract`)
 str(df11)
+
+df12[f_names] <- lapply(df12[f_names], factor)
+df12$`dateKontract` <- as.Date(df12$`dateKontract`)
+str(df12)
+
 rm(f_names)
 
 sum(is.na(df9$nameTz))
-sum(is.na(df11$nameTz))
+sum(is.na(df12$nameTz))
 
 df9 <- mutate(df9, month = format(dateKontract, "%m"))
 df11 <- mutate(df11, month = format(dateKontract, "%m"))
+df12 <- mutate(df12, month = format(dateKontract, "%m"))
 
 
-########
+write_xlsx(df9, "C:/Users/GudievZK/Desktop/Новая папка/df9.xlsx")
+write_xlsx(df11, "C:/Users/GudievZK/Desktop/Новая папка/df11.xlsx")
+write_xlsx(df12, "C:/Users/GudievZK/Desktop/Новая папка/df12.xlsx")
+
+############ Расчеты на основании выгрузки за 9 месяцев
 
 length(df9$status)                                # Количество закупок за 9 месяцев - 50473
 sum(df9$cost)                                     # Cумма закупок за 9 месяцев - 484 292 622 504
@@ -125,26 +138,58 @@ table(df9$nameTz) %>% sort(, decreasing = T, ) %>% .[1:3] # Топ 3 КТД за 9 месяц
                                                           # 3) Поставка канцелярских товаров, бумаги и бумажных изделий - 1905
 
 ############ Расчеты на основании выгрузки за 12-й месяц
-aggregate(cost ~ month, filter(df11, !is.na(df11$nameTz)), length)
-aggregate(cost ~ month, filter(df11, !is.na(df11$nameTz)), sum)
+aggregate(cost ~ month, filter(df12, !is.na(df12$nameTz)), length)
+aggregate(cost ~ month, filter(df12, !is.na(df12$nameTz)), sum)
+
+
+
 
 ############ Расчеты на основании выгрузки за 11 месяцев
-# df11 <- filter(df11, month != 12)
-aggregate(cost ~ month, filter(df11, !is.na(df11$nameTz) & month != 12), sum)
-aggregate(cost ~ month, filter(df11, !is.na(df11$nameTz) & month != 12), length)
+df11 <- filter(df12, month != 12)
+aggregate(cost ~ month, filter(df11, !is.na(df11$nameTz)), sum)
+aggregate(cost ~ month, filter(df11, !is.na(df11$nameTz)), length)
+
+aggregate(cost ~ month, df11, sum)
+aggregate(cost ~ month, df11, length)
+length(df11$status)
+sum(df11$cost)
 
 
-filter(df11, month != 12) %>% length(.$status)
+
+table(df11$nameTz) %>% sort(, decreasing = T, ) %>% .[1:3]  # Топ 3 КТД за 11 месяцев:
+                                                            # 1) Поставка лекарственных средств - 16427
+                                                            # 2) Поставка хозяйственных товаров - 3849
+                                                            # 3) Поставка канцелярских товаров, бумаги и бумажных изделий - 2408
 
 
-length(df11$status)                                # Количество закупок за 11 месяцев - 68924
-sum(df11$cost)                                     # Cумма закупок за 11 месяцев - 628 320 465 943
-length(df11$status[!is.na(df11$nameTz)])           # Количество закупок за 11 месяцев без NA - 68837 (68837 - 68924 = - 87)
-sum(df11$cost[!is.na(df11$nameTz)])                # Cумма закупок за 11 месяцев без NA - 623 254 664 799 (623254664799 - 628320465943 = - 5 065 801 144)
 
-table(df11$nameTz) %>% sort(, decreasing = T, ) %>% .[1:3] # Топ 3 КТД за 11 месяцев:
-                                                           # 1) Поставка лекарственных средств - 13087
-                                                           # 2) Поставка хозяйственных товаров - 3114
-                                                           # 3) Поставка канцелярских товаров, бумаги и бумажных изделий - 1905
+
+topCount <- df11 %>%
+            group_by(month, nameTz) %>%
+            summarise(cnt = n()) %>%
+            ungroup() %>%
+            group_by(month) %>%
+            top_n(1, cnt)
+
+
+topSum <- df11 %>%
+          group_by(month, nameTz) %>%
+          summarise(cnt = n(), sumCost = sum(cost)) %>%
+          ungroup() %>%
+          group_by(month) %>%
+          top_n(1, sumCost)
+
+topSum11 <- filter(df11, !is.na(df11$nameTz)) %>% 
+            group_by(nameTz) %>% 
+            summarise(cnt = n(), sumCost = sum(cost))
+
+
+length(df12$status)                                # Количество закупок за 11 месяцев - 68924
+sum(df12$cost)                                     # Cумма закупок за 11 месяцев - 628 320 465 943
+length(df12$status[!is.na(df12$nameTz)])           # Количество закупок за 11 месяцев без NA - 68837 (68837 - 68924 = - 87)
+sum(df12$cost[!is.na(df12$nameTz)])                # Cумма закупок за 11 месяцев без NA - 623 254 664 799 (623254664799 - 628320465943 = - 5 065 801 144)
+
+
+
 
 
