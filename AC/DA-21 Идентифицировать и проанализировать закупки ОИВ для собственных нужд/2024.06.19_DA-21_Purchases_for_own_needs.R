d47fa3f106fa54887c9f690e215bbaf8991c0431
Zelimkhan-Gudiev <gudievzk@gmail.com>
2024-06-21 18:56:39 +0300

    1

--- AC/DA-21 Идентифицировать и проанализировать закупки ОИВ для собственных нужд/2024.06.19_DA-21_Purchases_for_own_needs.R
+++ AC/DA-21 Идентифицировать и проанализировать закупки ОИВ для собственных нужд/2024.06.19_DA-21_Purchases_for_own_needs.R
@@ -7,26 +7,43 @@ library(stringr)
 library(purrr)
 library(rlang)
 
-df <- read_excel("C:/Users/GudievZK/Desktop/GitHub/DF/AC/2024.06.19_DA-22__MKR_Отчет по закупкам за 2023.xlsx")
+# Загрузка данных
+df <- read_excel("C:/Users/GudievZK/Desktop/GitHub/AC/DA-21 Идентифицировать и проанализировать закупки ОИВ для собственных нужд/2024.06.19_DA-21__MKR_Отчет по закупкам за 2023.xlsx")
 
+# Переименовывание колонок
 colnames(df) <- c("kod_kpgz", "name_kpgz", "is_standart", "grbs", "customer", "supplier", "name_tz", "tz_approval_date",
                   "using_tz", "notice_publication_date", "subject_of_procurement", "contract_date", "lot_id",
                   "contract_eis_registry_number", "contract_completion_date", "law", "method_of_determinig_supplier",
                   "Basis_for_concluding_a_contract_with_a_single_supplier", "object_status", "number_of_contracts",
                   "sum_of_contracts")
-df <- df[-c(1, 2),]
 
-df <- fill(df, "kod_kpgz", "name_kpgz", "is_standart", "grbs", "customer", "supplier", "name_tz", "tz_approval_date",
-           "using_tz", "notice_publication_date", "subject_of_procurement", "contract_date",                            ##"lot_id",
-           "contract_eis_registry_number", "contract_completion_date", "law", "method_of_determinig_supplier",
-           "Basis_for_concluding_a_contract_with_a_single_supplier", "object_status", "number_of_contracts",
-           "sum_of_contracts")
-df_temp <- filter(df, !str_detect(df$customer, "^ГБ|^\"ГБ|^ГК|^АО|^ГАУ|^ГУП|^ГАПОУ|^ГАОУ|^ООО|^«НИИ|^МАОУ|
+# Заполнение пустых ячеек
+df <- fill(df, "kod_kpgz", "name_kpgz", "is_standart", "grbs", "customer", "supplier", "name_tz", "tz_approval_date")
+
+# Удаление лишних столбцов и строк
+df <- df[-c(1, 2),]
+df <- filter(df, str_detect(df$kod_kpgz, "^01.15")) # удаляем позиции КПГЗ, не начинающиеся с 01.15
+
+# Считаем количество закупок, у которых нет рег. номера контракта/договрова
+df %>% filter(contract_eis_registry_number == "-") %>% distinct(lot_id, grbs, customer, supplier, name_tz, tz_approval_date,
+                                                                using_tz, notice_publication_date, subject_of_procurement, 
+                                                                contract_date, lot_id, contract_eis_registry_number, contract_completion_date, 
+                                                                law, method_of_determinig_supplier, 
+                                                                Basis_for_concluding_a_contract_with_a_single_supplier, 
+                                                                object_status, number_of_contracts,
+                                                                contract_eis_registry_number, .keep_all = T)
+
+# Считаем количество закупок, у которых есть рег. номер контракта/договрова
+df %>% filter(!contract_eis_registry_number == "-") %>% distinct(lot_id, contract_eis_registry_number, .keep_all = T) %>% nrow()
+
+
+df <- filter(df, !str_detect(df$customer, "^ГБ|^\"ГБ|^ГК|^АО|^ГАУ|^ГУП|^ГАПОУ|^ГАОУ|^ООО|^«НИИ|^МАОУ|
                                   |^СС и НМП|^ОАО|^«Мой особый семейный|^КП|^БЮРО|^ГПБУ|^МБУ|^МАДОУ|
                                   |^МАУ|^МУ |^МУК |^МОСКОВСКИЙ ГОСУДАРСТВЕННЫЙ ОБЪЕДИНЕННЫЙ МУЗЕЙ-ЗАПОВЕДНИК|
                                   |^Фонд реновации|^МГУУ Правительства Москвы, Университет Правительства Москвы|
                                   |^Московский фонд защиты прав дольщиков|^ЦПКиО им. М. Горького|
                                   |^«Московский театр «Современник»"))
+      
 
 
 
