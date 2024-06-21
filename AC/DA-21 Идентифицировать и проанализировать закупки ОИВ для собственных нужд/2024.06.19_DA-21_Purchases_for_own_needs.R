b22877a8af490f5e16eefbba9910901d90e8c489
Zelimkhan-Gudiev <gudievzk@gmail.com>
2024-06-21 15:27:57 +0300

    1

---
+++ AC/DA-21 Идентифицировать и проанализировать закупки ОИВ для собственных нужд/2024.06.19_DA-21_Purchases_for_own_needs.R
@@ -0,0 +1,33 @@
+library(readxl)
+library(dplyr)
+library(googlesheets4)
+library(googledrive)
+library(tidyverse)
+library(stringr)
+library(purrr)
+library(rlang)
+
+df <- read_excel("C:/Users/GudievZK/Desktop/GitHub/DF/AC/2024.06.19_DA-22__MKR_Îò÷åò ïî çàêóïêàì çà 2023.xlsx")
+
+colnames(df) <- c("kod_kpgz", "name_kpgz", "is_standart", "grbs", "customer", "supplier", "name_tz", "tz_approval_date",
+                  "using_tz", "notice_publication_date", "subject_of_procurement", "contract_date", "lot_id",
+                  "contract_eis_registry_number", "contract_completion_date", "law", "method_of_determinig_supplier",
+                  "Basis_for_concluding_a_contract_with_a_single_supplier", "object_status", "number_of_contracts",
+                  "sum_of_contracts")
+df <- df[-c(1, 2),]
+
+df <- fill(df, "kod_kpgz", "name_kpgz", "is_standart", "grbs", "customer", "supplier", "name_tz", "tz_approval_date",
+           "using_tz", "notice_publication_date", "subject_of_procurement", "contract_date",                            ##"lot_id",
+           "contract_eis_registry_number", "contract_completion_date", "law", "method_of_determinig_supplier",
+           "Basis_for_concluding_a_contract_with_a_single_supplier", "object_status", "number_of_contracts",
+           "sum_of_contracts")
+df_temp <- filter(df, !str_detect(df$customer, "^ÃÁ|^\"ÃÁ|^ÃÊ|^ÀÎ|^ÃÀÓ|^ÃÓÏ|^ÃÀÏÎÓ|^ÃÀÎÓ|^ÎÎÎ|^«ÍÈÈ|^ÌÀÎÓ|
+                                  |^ÑÑ è ÍÌÏ|^ÎÀÎ|^«Ìîé îñîáûé ñåìåéíûé|^ÊÏ|^ÁÞÐÎ|^ÃÏÁÓ|^ÌÁÓ|^ÌÀÄÎÓ|
+                                  |^ÌÀÓ|^ÌÓ |^ÌÓÊ |^ÌÎÑÊÎÂÑÊÈÉ ÃÎÑÓÄÀÐÑÒÂÅÍÍÛÉ ÎÁÚÅÄÈÍÅÍÍÛÉ ÌÓÇÅÉ-ÇÀÏÎÂÅÄÍÈÊ|
+                                  |^Ôîíä ðåíîâàöèè|^ÌÃÓÓ Ïðàâèòåëüñòâà Ìîñêâû, Óíèâåðñèòåò Ïðàâèòåëüñòâà Ìîñêâû|
+                                  |^Ìîñêîâñêèé ôîíä çàùèòû ïðàâ äîëüùèêîâ|^ÖÏÊèÎ èì. Ì. Ãîðüêîãî|
+                                  |^«Ìîñêîâñêèé òåàòð «Ñîâðåìåííèê»"))
+
+
+
+df1 <- df_temp %>% filter(str_detect(df_temp$subject_of_procurement, "äëÿ íóæä"))
